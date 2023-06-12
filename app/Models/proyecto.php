a9f5238357433c8e340c5f78158dc83074d57033
rafaelhernandezILP <rafael.hernandez@ilp.gob.sv>
2023-06-11 20:27:37 -0600

    preparando

---
+++ app/Models/proyecto.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use App\Http\Controllers\ProyectoController;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Proyecto
+ *
+ * @property $id
+ * @property $nombreproyecto
+ * @property $fuentefondos
+ * @property $montoplanificado
+ * @property $montopatrocinado
+ * @property $montofondospropios
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Proyecto extends Model
+{
+    
+    static $rules = [
+		'nombreproyecto' => 'required',
+		'fuentefondos' => 'required',
+		'montoplanificado' => 'required',
+		'montopatrocinado' => 'required',
+		'montofondospropios' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombreproyecto','fuentefondos','montoplanificado','montopatrocinado','montofondospropios'];
+
+
+
+}
